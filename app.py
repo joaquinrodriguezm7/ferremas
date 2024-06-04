a4982b79eef844543424c94d935e1983c7f0057f
TiBu132 <jfbadilla.mo@hotmail.com>
2024-06-03 21:57:31 -0400

    c

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from transbank.webpay.webpay_plus.transaction import Transaction, WebpayOptions
 from transbank.common.integration_type import IntegrationType
 from quart import Quart, request, jsonify, redirect, render_template
-import logging
-import httpx
+from urllib.parse import quote
+import logging, httpx, subprocess, datetime
+
 
 app = Quart(__name__)
 
@@ -12,23 +13,89 @@ api_key = '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'
 options = WebpayOptions(commerce_code, api_key, IntegrationType.TEST)
 transaction = Transaction(options)
 
-API_BASE_URL = 'http://localhost:8000/api/'
+BDD_API = 'http://localhost:8000/api/'
 
-@app.route('/')
-async def index():
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(API_BASE_URL + 'producto/')  # Endpoint de productos
+api_url= ''
 
-        if response.status_code == 200:
-            productos = response.json()
-            return await render_template('index.html', productos=productos)
+fecha_hoy = datetime.date.today()
+fecha_formateada = fecha_hoy.strftime("%Y-%m-%d")
+user='jfbadilla.mo@hotmail.com'
+password='Contraseña1'
+
+def construir_api_url(tipo_divisa):
+    fecha_hoy = datetime.date.today()
+    fecha_formateada = fecha_hoy.strftime("%Y-%m-%d")
+
+    if tipo_divisa == "dolar":
+        timeseries = "F073.TCO.PRE.Z.D"
+    elif tipo_divisa == "euro":
+        timeseries = "F072.CLP.EUR.N.O.D"
+    
+    else:
+        a=0
+
+    # Construir la URL codificando los parámetros
+    api_url = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={quote(password)}&function=GetSeries&timeseries={timeseries}&firstdate={fecha_formateada}&lastdate={fecha_formateada}"
+
+    
+    return api_url
+
+def valor_conversion(api_url):
+    response = httpx.get(api_url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        if "Series" in data and "Obs" in data["Series"]:
+            observaciones = data["Series"]["Obs"]
+            if len(observaciones) > 0:
+                valor_conversion = observaciones[0]["value"]
+                return float(valor_conversion)
+            else:
+                print("No se encontraron observaciones en la respuesta.")
         else:
-            return await jsonify({"error": "Error al obtener productos de la API de Django"}), response.status_code
-    except httpx.HTTPError as http_err:
-        return await jsonify({"error": f"HTTP error occurred: {http_err}"})
-    except Exception as err:
-        return jsonify({"error": f"Error occurred: {err}"}), 500
+            print("La estructura de la respuesta no es válida.")
+    else:
+        print(f"Error al obtener los datos. Código de estado: {response.status_code}")
+
+@app.route('/', methods=['POST', 'GET'])
+async def index():
+    if request.method == 'POST':
+        try:
+            data = await request.form
+            tipo_divisa = data.get('conversion')
+
+            if not tipo_divisa:
+                return jsonify({'error': 'Invalid data'}), 400
+            elif tipo_divisa == 'clp':
+                return jsonify({'tipo_divisa': tipo_divisa})
+            api_url = construir_api_url(tipo_divisa)
+            valor = valor_conversion(api_url)
+            
+            if valor is not None:
+                print(f'Valor de conversión obtenido: {valor}')
+
+            return jsonify({'tipo_divisa': tipo_divisa, 'valor_conversion': valor})
+        except Exception as e:
+            error = str(e)
+            print(error)
+            return jsonify({'error': 'Error en el servidor'}), 500
+        
+    else:
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(BDD_API + 'producto/')  # Endpoint de productos
+
+            if response.status_code == 200:
+                productos = response.json()
+                return await render_template('index.html', productos=productos)
+            else:
+                return await jsonify({"error": "Error al obtener productos de la API de Django"}), response.status_code
+        except httpx.HTTPError as http_err:
+            return await jsonify({"error": f"HTTP error occurred: {http_err}"})
+        except Exception as err:
+            return jsonify({"error": f"Error occurred: {err}"}), 500
+
+
 
 @app.route('/createWebpayTransaction', methods=['POST'])
 async def create_webpay_transaction():
