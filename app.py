29cc4f1c043bf12d6afca55fcb7c7bbbb4b494e3
TiBu132 <jfbadilla.mo@hotmail.com>
2024-06-04 23:03:52 -0400

    d

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from quart import Quart, request, jsonify, redirect, render_template
 from urllib.parse import quote
 import logging, httpx, subprocess, datetime
 
+#Declacación de variables
 
 app = Quart(__name__)
 
@@ -13,37 +14,36 @@ api_key = '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'
 options = WebpayOptions(commerce_code, api_key, IntegrationType.TEST)
 transaction = Transaction(options)
 
+
+#Variables para API's
 BDD_API = 'http://localhost:8000/api/'
-
 api_url= ''
-
 fecha_hoy = datetime.date.today()
 fecha_formateada = fecha_hoy.strftime("%Y-%m-%d")
 user='jfbadilla.mo@hotmail.com'
 password='Contraseña1'
 
+#Función para el llamado de la API del Banco Central
 def construir_api_url(tipo_divisa):
     fecha_hoy = datetime.date.today()
     fecha_formateada = fecha_hoy.strftime("%Y-%m-%d")
 
     if tipo_divisa == "dolar":
-        timeseries = "F073.TCO.PRE.Z.D"
+        api_url = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={quote(password)}&function=GetSeries&timeseries=F073.TCO.PRE.Z.D&firstdate={fecha_formateada}&lastdate={fecha_formateada}"
     elif tipo_divisa == "euro":
-        timeseries = "F072.CLP.EUR.N.O.D"
-    
+        api_url = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={quote(password)}&function=GetSeries&timeseries=F072.CLP.EUR.N.O.D&firstdate={fecha_formateada}&lastdate={fecha_formateada}"
     else:
-        a=0
+        api_url='sin API'
 
-    # Construir la URL codificando los parámetros
-    api_url = f"https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx?user={user}&pass={quote(password)}&function=GetSeries&timeseries={timeseries}&firstdate={fecha_formateada}&lastdate={fecha_formateada}"
-
-    
     return api_url
 
+#Función para recuperar el valor de conversión de la moneda seleccionada
 def valor_conversion(api_url):
     response = httpx.get(api_url)
-    
-    if response.status_code == 200:
+    if httpx.get(api_url)=='sin API':
+        valor_conversion=1  
+        return valor_conversion
+    elif response.status_code == 200:
         data = response.json()
         if "Series" in data and "Obs" in data["Series"]:
             observaciones = data["Series"]["Obs"]
@@ -57,43 +57,32 @@ def valor_conversion(api_url):
     else:
         print(f"Error al obtener los datos. Código de estado: {response.status_code}")
 
+
 @app.route('/', methods=['POST', 'GET'])
 async def index():
-    if request.method == 'POST':
-        try:
-            data = await request.form
-            tipo_divisa = data.get('conversion')
+    valor = 1
+    try:
+        #Llamada a la API de la BDD
+        async with httpx.AsyncClient() as client:
+            response = await client.get(BDD_API + 'producto/')
 
-            if not tipo_divisa:
-                return jsonify({'error': 'Invalid data'}), 400
-            elif tipo_divisa == 'clp':
-                return jsonify({'tipo_divisa': tipo_divisa})
+        #Se recupera la información del select para tipo de divisa
+        data = await request.form
+        tipo_divisa = data.get('conversion')
+    
+        if response.status_code == 200:
+            productos = response.json()
             api_url = construir_api_url(tipo_divisa)
+            if api_url == 'sin API':
+                return await render_template('index.html', productos=productos, valor=valor)
             valor = valor_conversion(api_url)
-            
-            if valor is not None:
-                print(f'Valor de conversión obtenido: {valor}')
-
-            return jsonify({'tipo_divisa': tipo_divisa, 'valor_conversion': valor})
-        except Exception as e:
-            error = str(e)
-            print(error)
-            return jsonify({'error': 'Error en el servidor'}), 500
-        
-    else:
-        try:
-            async with httpx.AsyncClient() as client:
-                response = await client.get(BDD_API + 'producto/')  # Endpoint de productos
-
-            if response.status_code == 200:
-                productos = response.json()
-                return await render_template('index.html', productos=productos)
-            else:
-                return await jsonify({"error": "Error al obtener productos de la API de Django"}), response.status_code
-        except httpx.HTTPError as http_err:
-            return await jsonify({"error": f"HTTP error occurred: {http_err}"})
-        except Exception as err:
-            return jsonify({"error": f"Error occurred: {err}"}), 500
+            return await render_template('index.html', productos=productos, valor=valor)
+        else:
+            return await jsonify({"error": "Error al obtener productos de la API de Django"}), response.status_code
+    except httpx.HTTPError as http_err:
+        return jsonify({"error": f"HTTP error occurred: {http_err}"})
+    except Exception as err:
+        return jsonify({"error": f"Error occurred: {err}"}), 500
 
 
 
