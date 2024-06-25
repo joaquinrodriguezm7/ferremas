7ab0b25713e2af39c2b76306b2514861ec7f250d
TiBu132 <jfbadilla.mo@hotmail.com>
2024-06-25 14:08:30 -0400

    e

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from transbank.webpay.webpay_plus.transaction import Transaction, WebpayOptions
 from transbank.common.integration_type import IntegrationType
-from quart import Quart, request, jsonify, redirect, render_template
+from quart import Quart, request, jsonify, redirect, render_template, session, url_for
 from urllib.parse import quote
+from collections import defaultdict
 import logging, httpx, subprocess, datetime
 
 #Declacación de variables
@@ -14,15 +15,44 @@ api_key = '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'
 options = WebpayOptions(commerce_code, api_key, IntegrationType.TEST)
 transaction = Transaction(options)
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+#Creación de filtros para formatear a valor monetario o fecha
+def format_datetime(transaction_date, format='%d-%m-%Y %H:%M:%S'):
+    """Formatea una fecha según el formato especificado."""
+    if transaction_date is None:
+        return ""
+    date = datetime.datetime.strptime(transaction_date, '%Y-%m-%dT%H:%M:%S.%fZ')
+    # Formatear la fecha según el formato dado
+    return date.strftime(format)
+
+def format_currency(amount):
+    return "{:,.0f}".format(amount).replace(",", ".")
+
+app.jinja_env.filters['format_currency'] = format_currency
+app.jinja_env.filters['format_datetime'] = format_datetime
 
 #Variables para API's
-BDD_API = 'https://0fc9-186-107-46-18.ngrok-free.app/api/'
+BDD_API = 'http://127.0.0.1:8000/api/'
 api_url= ''
 fecha_hoy = datetime.date.today()
 fecha_formateada = fecha_hoy.strftime("%Y-%m-%d")
 user='jfbadilla.mo@hotmail.com'
 password='Contraseña1'
 
+async def actualizar_stock_productos(productos):
+    async with httpx.AsyncClient() as client:
+        for p in productos:
+            response_stock= await client.get(f"{BDD_API}stock/{p}")
+            stock=response_stock.json()
+            for i in stock:
+                cantidad_stock = i['cantidad_stock']
+                print(f'Stock: {cantidad_stock}')
+                update_data_stock={
+                    'cantidad_stock' : cantidad_stock-1
+                }
+                update_response_stock = await client.put(f"{BDD_API}stock/{p}/actualizar", json=update_data_stock)
+        
 #Función para el llamado de la API del Banco Central
 def construir_api_url(tipo_divisa):
     fecha_hoy = datetime.date.today()
@@ -69,14 +99,15 @@ async def index():
         #Se recupera la información del select para tipo de divisa
         data = await request.form
         tipo_divisa = data.get('conversion')
-    
+        session['divisa']=tipo_divisa
         if response.status_code == 200:
             productos = response.json()
             api_url = construir_api_url(tipo_divisa)
             if api_url == 'sin API':
                 return await render_template('index.html', productos=productos, valor=valor)
             valor = valor_conversion(api_url)
-            return await render_template('index.html', productos=productos, valor=valor)
+            session['valor_conversion']=valor
+            return await render_template('index.html', productos=productos, valor=valor, tipo_divisa=tipo_divisa)
         else:
             return await jsonify({"error": "Error al obtener productos de la API de Django"}), response.status_code
     except httpx.HTTPError as http_err:
@@ -84,10 +115,153 @@ async def index():
     except Exception as e:
         return jsonify({"error": f"Error occurred: {e}"}), 500
 
-@app.route('/producto')
+#Creación de la transacción Webpay
+@app.route('/createWebpayTransaction', methods=['POST'])
+async def create_webpay_transaction():
+    form = await request.form
+    amount = form['webpay_total']
+    buy_order = 'orden12345678'
+    session_id = 'session12345678'
+    return_url = 'http://localhost:5000/getWebpayReturn'
+    nombre_producto = form['nombre_producto']
+    
+    productos = nombre_producto.split(',')
+
+    session['productos'] = productos
+
+    try:
+        response = transaction.create(buy_order, session_id, amount, return_url)
+        return redirect(response['url'] + '?token_ws=' + response['token'])
+    except Exception as error:
+        logging.error('Error creating Webpay transaction: %s', error)
+        return jsonify({"error": "Error creating Webpay transaction"}), 500
+
+
+#Retorno de transacción Webpay
+@app.route('/getWebpayReturn', methods=['GET'])
+async def get_webpay_return():
+    token_ws = request.args.get('token_ws')
+    if not token_ws:
+        return jsonify({"error": "token_ws es requerido"}), 400
+
+    try:
+        commit_response = transaction.commit(token_ws)
+        status=commit_response['status']
+        session['status']=status
+        session['resultado']=commit_response
+        return redirect('resultado.html')
+    except Exception as error:
+        logging.error('Error handling Webpay return: %s', error)
+        return jsonify({"error": "Error handling Webpay return", "a":f'error {error}'}), 500
+
+#Manejo de resultado para cliente
+@app.route('/resultado.html', methods=['GET','POST'])
+async def resultado():
+    status= session.get('status')
+    resultado= session.get('resultado')
+    productos=session.get('productos')
+    divisa=session.get('divisa')
+    valor=session.get('valor_conversion')
+    productos_count = defaultdict(int)
+
+    await actualizar_stock_productos(productos)
+
+    for producto in productos:
+        productos_count[producto] += 1
+    productos_con_contador = [(producto, count) for producto, count in productos_count.items()]
+    return await render_template('resultado.html', resultado=resultado, status=status, productos=productos_con_contador, divisa=divisa, valor=valor)
+
+#Opciones para crud de producto
+@app.route('/producto', methods=['GET','POST'])
 async def producto():
     return await render_template('producto.html')
 
+#Creación de producto
+@app.route('/createproducto', methods=['GET', 'POST'])
+async def createproducto():
+    try:
+        data = await request.form
+        nombre_producto=data.get('nombre_producto')
+        valor_producto=data.get('valor_producto')
+        descripcion_producto=data.get('descripcion_producto')
+        tipo_producto=data.get('tipo_producto')
+
+        create_data={
+                'nombre_producto':nombre_producto,
+                'valor_producto':valor_producto,
+                'descripcion_producto':descripcion_producto,
+                'tipo_producto':tipo_producto
+            }
+        async with httpx.AsyncClient() as client:
+            response_tp = await client.get(BDD_API + 'tipo_producto/')
+            response = await client.post(BDD_API + 'producto/agregar', json=create_data)
+        if response_tp.status_code == 200:
+            tipo_producto = response_tp.json()
+            return await render_template('createproducto.html', tipo_producto=tipo_producto)
+        if response.status_code == 200:
+            return jsonify({"Se ha creado correctamente el producto":f"{response}"})
+    except Exception as e:
+        return jsonify({"error": f"Error occurred: {e}"}), 500
+
+#Elección de producto para modificar
+@app.route('/updateproducto', methods=['GET', 'POST'])
+async def updateproducto():
+    try:
+        data = await request.form
+        async with httpx.AsyncClient() as client:
+            response = await client.get(BDD_API + 'producto/')
+        productos = response.json()
+
+        id_producto = data.get('id_producto')
+        nombre_producto = data.get('nombre_producto')
+
+        session['id_producto'] = id_producto
+        session['nombre_producto'] = nombre_producto
+        if "submit_button" in data:
+            return redirect(url_for('update'))
+        return await render_template('updateproducto.html', productos=productos)
+    except Exception as e:
+        return jsonify({'error':f'Error: {e}'})
+
+#Modificación de producto y stock por ID y Nombre
+@app.route('/update', methods=['GET', 'POST', 'PUT'])
+async def update():
+    try:
+        id_producto=session.get('id_producto')
+        nombre_producto=session.get('nombre_producto')
+        data = await request.form
+
+        valor_producto=data.get('valor_producto')
+        descripcion_producto=data.get('descripcion_producto')
+        cantidad_stock=data.get('cantidad_stock')
+
+        update_data_producto={
+            'valor_producto':valor_producto,
+            'descripcion_producto':descripcion_producto,
+        }
+
+        update_data_stock={
+            'cantidad_stock':cantidad_stock
+        }
+        if not id_producto:
+            return jsonify({'error': 'No product ID in session'})
+        
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"{BDD_API}producto/{id_producto}")
+            update_response_producto = await client.put(f"{BDD_API}producto/{id_producto}/actualizar", json=update_data_producto)
+            update_response_stock = await client.put(f"{BDD_API}stock/{nombre_producto}/actualizar", json=update_data_stock)
+
+        if response.status_code == 200:
+            producto=response.json()
+
+            if update_response_producto.status_code == 200 and update_response_stock.status_code == 200:
+               return await render_template('update.html', producto=producto) 
+            
+            return await render_template('update.html', producto=producto)
+    except Exception as e:
+        return jsonify({'error':f'Error: {e}'})
+    
+#Eliminación de producto por ID
 @app.route('/deleteproducto', methods=['GET', 'POST'])
 async def deleteproducto():
     try:
@@ -106,37 +280,8 @@ async def deleteproducto():
     except Exception as e:
         return jsonify({"error": f"Error occurred: {e}"}), 500
 
-@app.route('/createWebpayTransaction', methods=['POST'])
-async def create_webpay_transaction():
-    
-    form = await request.form
-    amount = form['amount']
-    
-    buy_order = 'orden12345678'
-    session_id = 'session12345678'
-    return_url = 'http://localhost:5000/getWebpayReturn'
-    
-    try:
-        response = transaction.create(buy_order, session_id, amount, return_url)
-        return redirect(response['url'] + '?token_ws=' + response['token'])
-    except Exception as error:
-        logging.error('Error creating Webpay transaction: %s', error)
-        return jsonify({"error": "Error creating Webpay transaction"}), 500
 
-@app.route('/getWebpayReturn', methods=['GET'])
-async def get_webpay_return():
-    token_ws = request.args.get('token_ws')
 
-    if not token_ws:
-        return jsonify({"error": "token_ws es requerido"}), 400
-
-    try:
-        commit_response = transaction.commit(token_ws)
-        return jsonify(commit_response)
-    except Exception as error:
-        logging.error('Error handling Webpay return: %s', error)
-        return jsonify({"error": "Error handling Webpay return"}), 500
-    
 if __name__ == '__main__':
     app.run(debug=True)
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,62 +1,127 @@
 let total_amount = 0;
+let lista = [];
+let webpay_total = 0;
+
 function comprarProducto() { 
-    document.getElementById('amount').value = total_amount;
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let productos = [];
+
+    cart.forEach(item => {
+        for (let i = 0; i < item.count; i++) {
+            productos.push(item.product);
+        }
+    });
+
+    document.getElementById('webpay_total').value = total_amount;
+    document.getElementById('nombre_producto').value = productos.join(',');
     document.getElementById('paymentForm').submit();
 }
+
+
 function guardarSeleccion() {
     var select = document.getElementById("conversion");
     var valorSeleccionado = select.value;
     localStorage.setItem("conversionSeleccionada", valorSeleccionado);
 }
-window.onload = function() {
+
+document.addEventListener('DOMContentLoaded', function() {
     var valorGuardado = localStorage.getItem("conversionSeleccionada");
     if (valorGuardado) {
         var select = document.getElementById("conversion");
         select.value = valorGuardado;
+    } else {
+        select.value = 'clp';
     }
-}
+    displayCart();
+});
 
-function addToCart(product, price) {
+function addToCart(product, price, quantityId) {
+    let quantityInput = document.getElementById(quantityId);
+    let quantity = parseInt(quantityInput.value);
+
+    if (isNaN(quantity) || quantity < 1) {
+        quantity = 1; // Establecer la cantidad predeterminada en 1 si no es válida
+    }
+
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.push({ product, price: parseFloat(price) });
+    
+    // Verificar si el producto ya está en el carrito
+    let productInCart = cart.find(item => item.product === product);
+    if (productInCart) {
+        productInCart.count += quantity;
+    } else {
+        cart.push({ product, price: parseFloat(price), count: quantity });
+    }
+
     localStorage.setItem('cart', JSON.stringify(cart));
     displayCart();
 }
 
+
 // Función para mostrar los productos del carrito
 function displayCart() {
     let total = 0;
+    let webpay_total = 0;
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
     let cartList = document.getElementById('cart');
     cartList.innerHTML = '';
+
     cart.forEach((item, index) => {
         if (!item.price) return; // Saltar si el precio no está definido
+        let convertedPrice = item.price / valor_conversion;
         let listItem = document.createElement('li');
-        listItem.textContent = `${item.product} - $${item.price.toFixed(2)}`;
+
+        if (!tipo_divisa) {
+            listItem.textContent = `x${item.count} ${item.product} - $${(convertedPrice * item.count).toFixed(2)}`;
+        } else if (tipo_divisa === 'dolar') {
+            listItem.textContent = `x${item.count} ${item.product} - $${(convertedPrice * item.count).toFixed(2)}`;
+        } else {
+            listItem.textContent = `x${item.count} ${item.product} - ${(convertedPrice * item.count).toFixed(2)}€`;
+        }
+
         let removeButton = document.createElement('button');
         removeButton.textContent = 'Eliminar';
         removeButton.onclick = function() { removeFromCart(index); };
         listItem.appendChild(removeButton);
         cartList.appendChild(listItem);
-        total += item.price;
+
+        total += convertedPrice * item.count;
+        webpay_total += item.price * item.count;
     });
-    total_amount = total;
-    document.getElementById('total').textContent = total.toFixed(2);
+
+    total_amount = webpay_total;
+    if (!tipo_divisa) {
+        document.getElementById('total').textContent = total.toFixed(2);
+    } else {
+        document.getElementById('total').textContent = total.toFixed(2);
+    }
+    document.getElementById('webpay_total').value = total_amount;
     document.getElementById('amount').value = total.toFixed(2);
 }
 
 // Función para eliminar productos del carrito
 function removeFromCart(index) {
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    cart.splice(index, 1);
+    if (cart[index].count > 1) {
+        cart[index].count -= 1;
+    } else {
+        cart.splice(index, 1);
+    }
     localStorage.setItem('cart', JSON.stringify(cart));
     displayCart();
 }
 
-// Mostrar el carrito al cargar la página
-window.onload = displayCart;
+function clearLocalStorage() {
+    localStorage.removeItem('cart');
+    localStorage.removeItem('conversionSeleccionada');
+    lista = [];
+}
 
-
+function clearCart() {
+    localStorage.removeItem('cart');
+    lista = [];
+    displayCart();
+}
 
 
 
